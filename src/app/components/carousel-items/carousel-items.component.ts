d900934e170d9fcc831be20139e12f0d21a0a2c8
PlopesK <popi081004@hotmail.com>
2023-08-11 17:28:19 -0300

    Added icons to carousel + carousel loop system :)

--- src/app/components/carousel-items/carousel-items.component.ts
+++ src/app/components/carousel-items/carousel-items.component.ts
@@ -3,33 +3,59 @@ import { Component } from '@angular/core';
 @Component({
 	selector: 'app-carousel-items',
 	templateUrl: './carousel-items.component.html',
-	styleUrls: ['./carousel-items.component.css']
+	styleUrls: ['./carousel-items.component.css'],
 })
 export class CarouselItemsComponent {
 	visibleItems: { text: string; icon: string }[] = [];
 	items = [
-		{ text: 'Home', icon: 'Icon 1' },
-		{ text: 'Conta Corrente', icon: 'Icon 2' },
-		{ text: 'Transferências', icon: 'Icon 3' },
-		{ text: 'Pagamentos', icon: 'Icon 4' },
-		{ text: 'Cartões', icon: 'Icon 5' },
-		{ text: 'Empréstimos', icon: 'Icon 6' },
-		{ text: 'Outros', icon: 'Icon 7' }
+		{
+			text: 'Home',
+			icon: 'https://www.santander.com.br/sites/WPC_CMS/imagem/22-08-16_181417_M_icon-compre-sua-casa.svg',
+		},
+		{
+			text: 'Conta Corrente',
+			icon: 'https://www.santander.com.br/sites/WPC_CMS/imagem/22-08-24_171817_M_icon-abra-sua-conta.svg',
+		},
+		{
+			text: 'Transferências',
+			icon: 'https://www.santander.com.br/sites/WPC_CMS/imagem/23-02-28_141604_M_ic_schedule_spending.svg',
+		},
+		{
+			text: 'Pagamentos',
+			icon: 'https://www.santander.com.br/sites/WPC_CMS/imagem/22-08-24_171509_M_icon-renegocie-sua-divida.svg',
+		},
+		{
+			text: 'Cartões',
+			icon: 'https://www.santander.com.br/sites/WPC_CMS/imagem/22-08-24_171937_M_icon-peca-seu-cartao.svg',
+		},
+		{
+			text: 'Empréstimos',
+			icon: 'https://www.santander.com.br/sites/WPC_CMS/imagem/22-08-16_180926_M_icon-emprestimo-para-voce.svg',
+		},
+		{
+			text: 'Outros',
+			icon: 'https://www.santander.com.br/sites/WPC_CMS/imagem/23-08-07_171932_M_ic_exchange_48.svg',
+		},
 	];
 	activeIndex = 0;
-
 
 	ngOnInit() {
 		this.updateVisibleItems();
 	}
 
 	updateVisibleItems() {
-		this.visibleItems = this.items.slice(this.activeIndex, this.activeIndex + 3);
+		this.visibleItems = this.items.slice(
+			this.activeIndex,
+			this.activeIndex + 3
+		);
 	}
 
 	moveLeft() {
 		if (this.activeIndex > 0) {
 			this.activeIndex--;
+			this.updateVisibleItems();
+		} else if (this.activeIndex == 0) {
+			this.activeIndex = this.items.length - 3;
 			this.updateVisibleItems();
 		}
 	}
@@ -37,6 +63,9 @@ export class CarouselItemsComponent {
 	moveRight() {
 		if (this.activeIndex < this.items.length - 3) {
 			this.activeIndex++;
+			this.updateVisibleItems();
+		} else if (this.activeIndex == this.items.length - 3) {
+			this.activeIndex = 0;
 			this.updateVisibleItems();
 		}
 	}
